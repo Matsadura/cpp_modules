83b576e83d2c55e85bae7a11ec919e9233a57ed5
Matsadura <zidanditoo1@gmail.com>
2025-11-19 15:42:40 +0100

    [ADD] ex02 (demistify references)

---
+++ C01/ex02/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+
+/**
+ * main - Entry of a program that showcases
+ *		the demystify references
+ */
+int	main( void )
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << \
+		"&str: " << &str <<
+		"\n&stringPTR: " << &stringPTR <<
+		"\n&stringRef: " << &stringREF <<
+		"\n\n";
+	
+	std::cout << \
+		"str: " << str <<
+		"\nstringPTR: " << *stringPTR <<
+		"\nstringRef: " << stringREF <<
+		"\n";
+
+	return (0);
+}
