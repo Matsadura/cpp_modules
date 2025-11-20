83c399a3e43e437a6c69651a09dacf4e5b5d3fa4
Matsadura <zidanditoo1@gmail.com>
2025-11-20 23:53:53 +0100

    [UPDATE] functions documentation

--- C01/ex03/HumanA.cpp
+++ C01/ex03/HumanA.cpp
@@ -1,18 +1,33 @@
 #include "HumanA.hpp"
 
+/**
+ * HumanA - Class constructor
+ * @name: HumanA's name
+ * @weapon: Referance to weapon
+ */
 HumanA::HumanA( std::string name, Weapon& weapon ) : _name(name), _weapon(weapon)
 {
 }
 
+/**
+ * Class destructor
+ */
 HumanA::~HumanA( void )
 {
 }
 
+/**
+ * getWeapon - Getter for weapon attribute
+ * Return: Weapon
+ */
 Weapon const& HumanA::getWeapon( void ) const
 {
 	return (this->_weapon);
 }
 
+/**
+ * attack - Prints attack statement
+ */
 void HumanA::attack( void )
 {
 	std::cout
--- C01/ex03/HumanB.cpp
+++ C01/ex03/HumanB.cpp
@@ -1,25 +1,43 @@
 #include "HumanB.hpp"
 
+/**
+ * HumanB - Class constactor
+ * @name: Human's name
+ */
 HumanB::HumanB( std::string name )
 {
 	this->_name = name;
 	this->_weapon = NULL;
 }
 
+/**
+ * Class destructor
+ */
 HumanB::~HumanB( void )
 {
 }
 
+/**
+ * setWeapon - Setter for weapon attribute
+ * @weapon: Reference to weapon
+ */
 void	HumanB::setWeapon( Weapon& weapon )
 {
 	this->_weapon = &weapon;
 }
 
+/**
+ * getWeapon - Getter for weapon
+ * Return: Pointer to weapon object
+ */
 Weapon* HumanB::getWeapon( void ) const
 {
 	return (this->_weapon);
 }
 
+/**
+ * attack - Prints attack statement
+ */
 void HumanB::attack( void )
 {
 	std::cout
